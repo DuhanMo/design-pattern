a10f2656af44219155705aacd9be48575a965cd2
Duhan Mo <moduhan95@gmail.com>
2023-07-20 14:54:00 +0900

    Refactor ingredientFactory to variable

--- src/main/kotlin/_04_factory/good/abstractfactory/KiaCarFactory.kt
+++ src/main/kotlin/_04_factory/good/abstractfactory/KiaCarFactory.kt
@@ -5,10 +5,12 @@ import _04_factory.good.abstractfactory.car.Carnival
 import _04_factory.good.abstractfactory.car.Stinger
 
 class KiaCarFactory : CarFactory() {
+    private val ingredientFactory = KiaCarIngredientFactory()
+
     override fun createCar(type: String): Car {
         val car = when (type) {
-            "SUV" -> Carnival(KiaCarIngredientFactory())
-            "SPORTS" -> Stinger(KiaCarIngredientFactory())
+            "SUV" -> Carnival(ingredientFactory)
+            "SPORTS" -> Stinger(ingredientFactory)
             else -> throw IllegalArgumentException("그런 차는 없어요")
         }
         return car
--- src/main/kotlin/_04_factory/good/abstractfactory/TeslaCarFactory.kt
+++ src/main/kotlin/_04_factory/good/abstractfactory/TeslaCarFactory.kt
@@ -6,10 +6,12 @@ import _04_factory.good.abstractfactory.car.Roadster
 
 
 class TeslaCarFactory : CarFactory() {
+    private val ingredientFactory = TeslaCarIngredientFactory()
+
     override fun createCar(type: String): Car {
         val car = when (type) {
-            "SUV" -> ModelX(TeslaCarIngredientFactory())
-            "SPORTS" -> Roadster(TeslaCarIngredientFactory())
+            "SUV" -> ModelX(ingredientFactory)
+            "SPORTS" -> Roadster(ingredientFactory)
             else -> throw IllegalArgumentException("그런 차는 없어요")
         }
         return car
